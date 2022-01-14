9c09fb120c46e9a142758238f8602bbd9b32ffbb
Kaura Peura <kaura.peura@gmail.com>
2022-01-15 00:43:20 +0200

    🔨 Fix a bug in the build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p dist
 cp -f src/index.html dist
 cp -f src/styles.css dist
-cp -fr assets dist/assets
+cp -fr assets dist
 
 npx tsc -noemit
 npx esbuild src/main.ts --bundle --format=esm --outdir=dist --splitting
